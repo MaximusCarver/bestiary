ae10a5fa638cf12f3c784ce85d36b84fbbdec3de
MoonLord2436 <97262816+MoonLord2436@users.noreply.github.com>
2023-10-01 11:35:43 -0700

    Upload Bestiary version 1

---
+++ BestiaryV1/src/Weapon.java
@@ -0,0 +1,11 @@
+public class Weapon {
+    String name;
+    int damage;
+    int sellPrice;
+
+    public Weapon(String name, int damage, int sellPrice) {
+        this.name = name;
+        this.damage = damage;
+        this.sellPrice = sellPrice;
+    }
+}
